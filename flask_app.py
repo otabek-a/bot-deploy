2a4919d69c89e5577e97a52dfdada2595b36e078
khakimovallamurod <allamurod0206@gmail.com>
2025-03-18 18:15:58 +0200

    add route

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,16 @@
-from flask import Flask 
+from flask import Flask, request
 
 app = Flask(__name__)
 @app.route('/')
 def main():
     return "Hello GitHub"
+
+@app.route('/get/sum')
+def get_sum():
+    query = request.args
+    a = query.get('a', 0)
+    b = query.get('b', 0)
+    return {"sum" : int(a) + int(b)}
 
 if __name__ == "__main__":
     app.run(debug=True)
