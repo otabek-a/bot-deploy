189f5fa6b547ea216c3a3b473c7bb5a3a5b1bef8
khakimovallamurod <allamurod0206@gmail.com>
2025-03-25 18:29:49 +0200

    send message

--- flaskapp.py
+++ flaskapp.py
@@ -3,16 +3,22 @@ from telegram import Bot
 import config
 
 app = Flask(__name__)
-TOKEN = config.get_token()
+TOKEN = "7214563574:AAH7aOfSckAeK8iSRR5iCMCuMahIYDDqxz4"
 bot = Bot(token=TOKEN)
 
 @app.route('/', methods=["POST"])
 def main():
     data = request.get_json()
-
+    chat_id = data['message']['from']['id']
+    text = data['message']['text']
+    first_name = data['message']['from']['first_name']
+    if text == '/start':
+        bot.send_message(chat_id=chat_id, text= f"Hello {first_name}")
+    else:
+        bot.send_message(chat_id=chat_id, text= text)
     print(data)
 
-    
+
     return data
 
 @app.route('/home')
